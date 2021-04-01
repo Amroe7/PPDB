0d63f0bdab9cc662ee2dbb8f729c3bc62aba5d23
Amroe7 <muhamad.amroe@gmail.com>
2021-04-01 11:40:07 +0700

    Project Latihan

---
+++ resources/views/siswas/edit.blade.php
@@ -0,0 +1,91 @@
+@extends('siswas.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Edit Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('siswas.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="{{ route('siswas.update',$siswa->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIS:</strong>
+                <input type="text" name="NIS" value="{{ $siswa->NIS }}" class="form-control" placeholder="nis">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="Nama" value="{{ $siswa->Nama }}" class="form-control" placeholder="nama">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jenis Kelamin:</strong>
+                <input type="text" name="jns_kelamin" value="{{ $siswa->jns_kelamin }}" class="form-control" placeholder="Jenis kelamin">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tempat lahir:</strong>
+                <input type="text" name="tempat_lahir" value="{{ $siswa->tempat_lahir }}" class="form-control" placeholder="Tempat lahir">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tanggal Lahir:</strong>
+                <input type="text" name="tanggal_lahir" value="{{ $siswa->tanggal_lahir }}" class="form-control" placeholder="Tanggal lahir">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <textarea class="form-control" style="height:150px" name="alamat" placeholder="alamat">{{ $siswa->alamat }}</textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Asal Sekolah:</strong>
+                <input type="text" name="asal_sekolah" value="{{ $siswa->asal_sekolah }}" class="form-control" placeholder="Asal Sekolah">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kelas:</strong>
+                <input type="text" name="kelas" value="{{ $siswa->kelas }}" class="form-control" placeholder="Kelas">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jurusan:</strong>
+                <input type="text" name="jurusan" value="{{ $siswa->jurusan }}" class="form-control" placeholder="Jurusan">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
---
+++ resources/views/siswas/index.blade.php
@@ -0,0 +1,66 @@
+@extends('siswas.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Laravel 8 CRUD Example from scratch - ItSolutionStuff.com</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="{{ route('siswas.create') }}"> Create New Product</a>
+        </div>
+    </div>
+</div>
+
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
+@endif
+
+<table class="table table-bordered">
+    <tr>
+        <th>NIS</th>
+        <th>Nama</th>
+        <th>Jenis Kelamin</th>
+        <th>Tempat Lahir</th>
+        <th>tanggal Lahir</th>
+        <th>Alamat</th>
+        <th>Asal Sekolah</th>
+        <th>kelas</th>
+        <th>Jurusan</th>
+        <th width="280px">Action</th>
+    </tr>
+    @foreach ($siswas as $siswa)
+    <tr>
+        <td>{{ ++$i }}</td>
+        <td>{{ $siswa->NIS }}</td>
+        <td>{{ $product->Nama }}</td>
+        <td>{{ $siswa->jns_kelamin }}</td>
+        <td>{{ $siswa->tempat_lahir }}</td>
+        <td>{{ $siswa->tanggal_lahir }}</td>
+        <td>{{ $siswa->alamat }}</td>
+        <td>{{ $siswa->asal_sekolah }}</td>
+        <td>{{ $siswa->kelas }}</td>
+        <td>{{ $siswa->jurusan }}</td>
+
+        <td>
+            <form action="{{ route('siswas.destroy',$siswa->id) }}" method="POST">
+
+                <a class="btn btn-info" href="{{ route('siswas.show',$siswa->id) }}">Show</a>
+
+                <a class="btn btn-primary" href="{{ route('siswas.edit',$siswa->id) }}">Edit</a>
+
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+{!! $siswas->links() !!}
+
+@endsection
